57e9f9550820d047cddbf8d524d17a4882073e59
prfigueiredo <paulofigueiredo@id.uff.br>
2023-03-22 21:35:56 -0300

    View and Presenter almost done

--- lib/Presenter/presenter_page.dart
+++ lib/Presenter/presenter_page.dart
@@ -1,4 +1,44 @@
+import 'package:flutter/material.dart';
+
 // Presenter (Model-View-Presenter pattern)
 class Presenter{
+  //Vars
+  late String picture = 'assets/remote.svg';
+  late String info =  "- Let me know a lil' bit about you...";
+  double? tempo;
+
+  //Controllers
+  TextEditingController goController = TextEditingController();
+  TextEditingController backController = TextEditingController();
+  TextEditingController elevatorController = TextEditingController();
+
+  //A method that calculates spent time by the user.
+  void calculate(){
+    double go = double.parse(goController.text.replaceAll(',', '.'));
+    double back = double.parse(backController.text.replaceAll(',', '.'));
+    double elevator = double.parse(elevatorController.text.replaceAll(',', '.'));
+    //22 worked by month - 12 months worked per year
+    tempo = ((go + back + elevator) * 22/60*12)/24;
+  }
+
+  //A method to change the SVG
+  void change(){
+    picture = 'assets/stressedout.svg';
+    if(tempo == null) {
+      info = "Error loading data...";
+      print(tempo);
+    }else{
+      info = "Gosh! You saved ${tempo.toString()} days per year of your life!";
+    }
+  }
+
+  //A method to refresh information
+  void refresh(){
+    goController.clear();
+    backController.clear();
+    elevatorController.clear();
+    picture = 'assets/remote.svg';
+    info =  "- Let me know a lil' bit about you...";
+  }
 
 }
--- lib/View/home_page.dart
+++ lib/View/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:time_saver/Presenter/presenter_page.dart';
 
 //View (Model-View-Presenter pattern)
 class Home extends StatefulWidget {
@@ -10,6 +11,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //Instancing Presenter
+  Presenter c = Presenter();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +24,17 @@ class _HomeState extends State<Home> {
         ),
         backgroundColor: Colors.orangeAccent,
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                c.refresh();
+              });
+            },
+            icon: Icon(Icons.refresh),
+          ),
+          const SizedBox(width: 10),
+        ],
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
@@ -28,22 +43,78 @@ class _HomeState extends State<Home> {
           children: [
             const Divider(),
             SvgPicture.asset(
-              'assets/remote.svg',
               height: 255,
+              c.picture,
             ),
-            const Divider(),
+            const SizedBox(height: 10),
+            Center(
+                child: Text(
+              c.info,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Colors.indigoAccent),
+            )),
+            const SizedBox(
+              height: 22,
+            ),
             Text(
               'How much time do you spend going to the office?',
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
             ),
-            const Divider(),
+            const SizedBox(height: 10),
             TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: 'MINUTES(MINS)',
               ),
               textAlign: TextAlign.center,
+              controller: c.goController,
             ),
+            const Divider(),
+            Text(
+              'And on the way home?',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'MINUTES(MINS)',
+              ),
+              textAlign: TextAlign.center,
+              controller: c.backController,
+            ),
+            const Divider(),
+            Text(
+              '...waiting the elevator?',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'MINUTES(MINS)',
+              ),
+              textAlign: TextAlign.center,
+              controller: c.elevatorController,
+            ),
+            SizedBox(
+                height: 50,
+                width: 50,
+                child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        c.calculate();
+                        c.change();
+                      });
+                    },
+                    child: Text(
+                      'VERDICT!',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ))),
+            const Divider(),
           ],
         ),
       ),
